ce0c85905f5a1aa25918447ad1b7a61cebf0cc13
sunghyuk.kay <sunghyuk.kay@kaist.ac.kr>
2020-10-08 19:35:55 +0900

    Update missing commands for init-nvim.sh

--- nvim/init-nvim.sh
+++ nvim/init-nvim.sh
@@ -1,1 +1,9 @@
-nvim -c "CocInstall $(paste -sd ' ' $HOME/dotfiles/nvim/coc-extensions.txt)"
+# Download vim-plug first
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# TODO  Linking for Windows?
+ln -nfs $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+
+nvim -c "PlugInstall"-c "CocInstall $(paste -sd ' ' $HOME/dotfiles/nvim/coc-extensions.txt)"
+
+# TODO quit nvim after installation?
