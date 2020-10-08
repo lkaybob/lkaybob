17cd8bcb3706ed5274ec337472be3d670962d79a
Lawrence Kay <me@lkaybob.pe.kr>
2020-10-09 00:48:46 +0900

    Additional minor changes * Typos on ubuntu-server/packages.txt * Test and create /Users/lkaybob/.config/nvim directory

--- nvim/init-nvim.sh
+++ nvim/init-nvim.sh
@@ -2,7 +2,10 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # TODO  Linking for Windows?
-ln -nfs $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+
+if [ ! -d $HOME/.config/nvim ]; then
+  ln -nfs $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+fi
 
 nvim -c "PlugInstall" -c "CocInstall $(paste -sd ' ' $HOME/dotfiles/nvim/coc-extensions.txt)"
 
---
+++ ubuntu-server/README.md
@@ -0,0 +1,2 @@
+Package names are based on Ubuntu 20.04.
+* Some packages' names are difference (ex. "gnugpg-agent" is "gpg-agent" on 18.04")
