fe69dd920de077ab17eaf3e2718271daa222172c
Lawrence Kay <me@lkaybob.pe.kr>
2022-02-24 21:23:50 +0900

    Update zsh init script * With update on OSX init script

--- scripts/bootstrap-osx.sh
+++ scripts/bootstrap-osx.sh
@@ -6,21 +6,13 @@ fi
 # Brew setup
 brew update
 brew tap homebrew/bundle
-brew bundle --file=$HOME/dotfiles/osx/Brewfile
+brew bundle --file=$HOME/lkaybob/osx/Brewfile
 
 # Clean up the caches
 brew cleanup
 brew cask cleanup
 
-# TODO Replace with zsh/init.sh
-# Set the zsh as primary shell
-chsh -s $(which zsh)
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-ln -nfs $HOME/dotfiles/zsh/themes/bullet-train/bullet-train.zsh-theme $HOME/.oh-my-zsh/themes/bullet-train.zsh-theme
-
-ln -nfs $HOME/dotfiles/osx/.bash_profile $HOME/.bash_profile
-
-[ ! -f $HOME/.zshrc ] && ln -nfs $HOME/dotfiles/.zshrc $HOME/.zshrc
-source $HOME/.zshrc
+# Init zsh
+$HOME/lkaybob/zsh/init.sh
 
 # TODO nvim initialization should be performed at last.
--- zsh/init.sh
+++ zsh/init.sh
@@ -1,17 +1,19 @@
 # TODO Test if zsh exists
-
-# Install oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" --skip-chsh
-
-# Synchronize bullet-train theme
-cd $HOME/lkaybob/zsh
-git submodule init
-git submodule update
-ln -nfs $HOME/lkaybob/zsh/themes/bullet-train/bullet-train.zsh-theme $HOME/.oh-my-zsh/themes/bullet-train.zsh-theme
-cd $HOME/lkaybob
-
-ln -nfs $HOME/lkaybob/osx/.bash_profile $HOME/.bash_profile
-
-[ ! -f $HOME/.zshrc ] && rm $HOME/.zshrc
-ln -nfs $HOME/lkaybob/.zshrc $HOME/.zshrc
-source $HOME/.zshrc
+if [[ -x $(which zsh) ]]
+then
+  # Install oh-my-zsh
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" --skip-chsh
+  
+  # Synchronize bullet-train theme
+  cd $HOME/lkaybob/zsh
+  git submodule init
+  git submodule update
+  ln -nfs $HOME/lkaybob/zsh/themes/bullet-train/bullet-train.zsh-theme $HOME/.oh-my-zsh/themes/bullet-train.zsh-theme
+  cd $HOME/lkaybob
+  
+  ln -nfs $HOME/lkaybob/osx/.bash_profile $HOME/.bash_profile
+  
+  [ ! -f $HOME/.zshrc ] && rm $HOME/.zshrc
+  ln -nfs $HOME/lkaybob/.zshrc $HOME/.zshrc
+  source $HOME/.zshrc
+fi
